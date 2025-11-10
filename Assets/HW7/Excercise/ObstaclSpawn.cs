7443c3fa773b2d2a5fa2f883a1d2eba12ba0e0dd
Kai13131 <haiyang.he13@gmail.com>
2025-11-10 02:03:21 -0500

    hw7

---
+++ Assets/HW7/Excercise/ObstaclSpawn.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ObstaclSpawn : MonoBehaviour
+{
+    public GameObject obstaclPrefab;
+    public int spawnCount = 5;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
